22c33d26a95fb811ce6dd81d1dbcd83835a82472
Mehdi Zareian <139323872+Mehdi-Sussy@users.noreply.github.com>
2023-07-25 17:28:20 +0330

    minor change Js-02.js

--- js-02/Js-02.js
+++ js-02/Js-02.js
@@ -4,9 +4,9 @@ const leftNumber = +prompt("enter number!")
 const operator = prompt("enter sign!")
 const rightNumber = +prompt("enter number!")
 
-if (isNaN(rightNumber || leftNumber)){
-     console.log("error in left or right number data type!")
- }
+if (isNaN(leftNumber) || isNaN(rightNumber)){
+    console.log("error in left or right number data type!")
+
 
 if (operator === "+") {
     document.writeln(leftNumber + rightNumber)
