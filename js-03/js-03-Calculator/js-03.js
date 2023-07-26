14b93780b9092de426e48379b2558c0b2f8f4b74
Mehdi Zareian <139323872+Mehdi-Sussy@users.noreply.github.com>
2023-07-26 10:16:19 +0330

    deleted comments and rename some values

--- js-03/js-03-Calculator/js-03.js
+++ js-03/js-03-Calculator/js-03.js
@@ -1,30 +1,7 @@
-// const x = +prompt("e")
-// const operator = prompt("enter sign!")
-// const y = +prompt("e2!")
-// if (isNaN(x + y)){
-//     console.log("Error")
-// } else {
-//     switch (operator){
-//         case "+": console.log(x+y)
-//         break;
-//     }
-//     switch (operator){
-//         case "-": console.log(x-y)
-//         break;
-//     }
-//     switch (operator){
-//         case "*": console.log(x*y)
-//         break;
-//     }
-//     switch (operator){
-//         case "/": console.log(x/y)
-//         break;
-//     }
-// }
 
-const Entry1 = +prompt("enter ur shit")
-const Entry2 = +prompt("enter ur shit2")
-const operator = prompt("enter ur shit3")
+const Entry1 = +prompt("enter ur number")
+const Entry2 = +prompt("enter ur number2")
+const operator = prompt("enter operator")
 
 function Calculator(Entry1 = 0, Entry2 = 0, operator){
 if (!isNaN(Entry1 + Entry2)){
