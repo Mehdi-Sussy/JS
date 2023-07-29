009106c3a622b4725579a3e98725990fec5443fb
Mehdi.Zareiane <affrosamuraii@gmail.com>
2023-07-29 10:51:46 +0330

    feat/Mehdi-Sussy/Added js-04 HW except Object part and readme files

---
+++ js-04/CelsiusToFahrenheitConvertor/js-04.js
@@ -0,0 +1,8 @@
+
+function CelsiusToFahrenheitConvertor(Celsius) {
+
+Fahrenheit = (Celsius*(9/5))+32
+
+return Fahrenheit + "F"
+}
+console.log(CelsiusToFahrenheitConvertor(10))
---
+++ js-04/Object/js-04.js
@@ -0,0 +1,9 @@
+const productProperties= {
+
+  name:"Phone",
+  price:"1800$",
+  model: 1402,
+  OS:12,
+
+
+}
