59551ec7c4de73b2368ddb1487339d30f821a808
Mehdi.Zareiane <affrosamuraii@gmail.com>
2023-08-22 12:08:18 +0330

    feat/Mehdi-Sussy/edits and added readme file.

--- js-06/js-06.js
+++ js-06/js-06.js
@@ -2,20 +2,23 @@ const array = [{
     name:"Mehdi",
     lastName:"Zareian",
     age:20 ,
+    major: "Maths",
     id: self.crypto.randomUUID()
 },{
     name:"ALi",
     lastName:"Rezaii",
     age:20 ,
+    major: "Physics",
     id: self.crypto.randomUUID(),
 
 }]
-const addFunction = (name,lastName,age) => {
-if (!name || !lastName || !age) return null
+const addFunction = (name,lastName,age,major) => {
+if (!name || !lastName || !age || !major) return null
 const newUser = {
     name,
     lastName,
     age,
+    major,
     id: self.crypto.randomUUID()
 }
 array.push(newUser)
@@ -25,7 +28,7 @@ const popFunction = ()=> {
 array.pop()
 }
 console.log(array)
-addFunction("hello","zawg",25)
+addFunction("hello","zawg",25,"science")
 const indexToEdit = 2;
 if (indexToEdit >= 0 && indexToEdit < array.length) {
     const objectToEdit = array[indexToEdit];
---
+++ js-06/readme.md
@@ -0,0 +1,17 @@
+# HW number 6 of javaScript
+
+## descriptions: 
+1- Write an array of users(each are object).
+2- then write function which can add, remove a user and or edit its properties.
+3- each person should have name, age, id and major.
+
+### procedure and challenges: 
+First i start off with writing my array with 2 default persons (object) insde.
+then i went on and create an addFunction which is able to add (push method) a person with same
+properties as other into the array.
+its also possible to define a variable object and then inster the variable (person) in the array.
+
+Then i write a function to delete a person from the object with pop() method. 
+problem of this method is that you can't delete a specefic person.
+
+Then i asked chatgpt for the last segment of HW which i couldn't solve and it solve it with a if condition.
